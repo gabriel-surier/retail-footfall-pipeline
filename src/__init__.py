927a05582280b55dd45ad06990323e2d42b487c3
Gabriel SURIER <gabriel.surier@gmail.com>
2026-07-31 11:31:56 +0200

    chore(ci): correcting __init__.py with standard lint, typo and format.

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,12 @@
-fr"""
+"""
 @File    :   __init__.py
 @Time    :   2020/07/30
 @Author  :   Gabriel SURIER
 @Purpose :   Declare base app for api usage 
 """
-from datetime import date
 
 from src.sensor import AttendanceSensor
+
 
 def create_app() -> dict:
     """
@@ -14,19 +14,19 @@ def create_app() -> dict:
     4 doors for this store so 4 sensors
     """
     door_name = ["north", "south", "east", "west"]
-    door_avg_visit = [100,250,150,400]
-    door_std_visit = [25,75,40,100]
-    pct_dysfunction= [0.05,0.08,0.1,0.075]
-    pct_breakdown = [0.01,0.03,0.05,0.02]
+    door_avg_visit = [100, 250, 150, 400]
+    door_std_visit = [25, 75, 40, 100]
+    pct_dysfunction = [0.05, 0.08, 0.1, 0.075]
+    pct_breakdown = [0.01, 0.03, 0.05, 0.02]
 
-    door_dict = dict()
+    door_dict: dict = {}
 
-    for i in range(len(door_name)):
-        door_dict[door_name[i]] = AttendanceSensor(
-            door_avg_visit[i],
-            door_std_visit[i],
-            pct_dysfunction[i],
-            pct_breakdown[i],
+    for i in enumerate(door_name):
+        index=i[0]
+        door_dict[door_name[index]] = AttendanceSensor(
+            door_avg_visit[index],
+            door_std_visit[index],
+            pct_dysfunction[index],
+            pct_breakdown[index],
         )
     return door_dict
-
